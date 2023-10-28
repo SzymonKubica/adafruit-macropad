2df12aa876207e796fe013b27136e5fbd9d15ef2
SzymonKubica <szymo.kubica@gmail.com>
2023-10-28 21:25:59 +0100

    Update the media to get rid of the not needed brightness adjustments.

--- macros/media.py
+++ macros/media.py
@@ -24,13 +24,13 @@ app = {               # REQUIRED dict, must be named 'app'
     'macros' : [      # List of button macros...
         # COLOR    LABEL    KEY SEQUENCE
         # 1st row ----------
-        (0x202020, 'PrtSc', [Keycode.PRINT_SCREEN]),
-        (0x000020, 'Vol+', [[ConsumerControlCode.VOLUME_INCREMENT]]),
-        (0x202020, 'Bright+', [[ConsumerControlCode.BRIGHTNESS_INCREMENT]]),
-        # 2nd row ----------
         (0x202020, 'Lock', [Keycode.WINDOWS, Keycode.L]),
+        (0x000020, 'Vol+', [[ConsumerControlCode.VOLUME_INCREMENT]]),
+        (0x202020, 'PrtSc', [Keycode.PRINT_SCREEN]),
+        # 2nd row ----------
+        (0x000000, '', []),
         (0x000020, 'Vol-', [[ConsumerControlCode.VOLUME_DECREMENT]]),
-        (0x202020, 'Bright-', [[ConsumerControlCode.BRIGHTNESS_DECREMENT]]),
+        (0x000000, '', []),
         # 3rd row ----------
         (0x000000, '', []),
         (0x200000, 'Mute', [[ConsumerControlCode.MUTE]]),
@@ -40,6 +40,6 @@ app = {               # REQUIRED dict, must be named 'app'
         (0x002000, 'Play/Pause', [[ConsumerControlCode.PLAY_PAUSE]]),
         (0x202000, '>>', [[ConsumerControlCode.SCAN_NEXT_TRACK]]),
         # Encoder button ---
-        (0x000000, '', [])
+        (0x000000, '', [Keycode.ESCAPE])
     ]
 }
